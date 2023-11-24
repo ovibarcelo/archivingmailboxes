9e83f3d3a96526d79bd1bfe5c628b49b43d4d149
ovibarcelo <49959594+ovibarcelo@users.noreply.github.com>
2023-11-24 13:44:56 +0100

    Add files via upload

---
+++ Mailboxes archiving.ps1
@@ -0,0 +1,27 @@
+﻿Connect-ExchangeOnline
+#Get the retention policy applied to a user
+##Get-Mailbox -Identity admin@edu.educaronline.org | Select-Object DisplayName, RetentionPolicy
+
+#Get the archive mailbox status for a user
+##Get-Mailbox -Identity adelev@edu.educaronline.org | Select ArchiveStatus, ArchiveDatabase
+
+#Get the Srchive status for all
+##Get-Mailbox -ResultSize Unlimited -RecipientTypeDetails UserMailbox | Select DisplayName, ArchiveStatus, ArchiveDatabase, RetentionPolcicy
+
+#Get the policy aplied to all
+##Get-Mailbox -ResultSize Unlimited -RecipientTypeDetails UserMailbox | Select-Object DisplayName, RetentionPolcicy
+
+
+
+# Get the mailboxes that belong to the group "Students"
+$mailboxes = Get-Recipient -Filter {Title -eq "Student"}
+
+
+# Get the retention policy named "Bruno's policy"
+$policy = Get-RetentionPolicy -Identity "Archive 1 year"
+
+# Apply the retention policy to the mailboxes
+$mailboxes | ForEach-Object {Set-Mailbox -Identity $_.Identity -RetentionPolicy $policy.Name}
+
+# Verify the policy assignment and export the output to a CSV file
+$mailboxes | ForEach-Object {Get-Mailbox -Identity $_.Identity | Select-Object DisplayName, RetentionPolicy} | Export-CSV "C:\PolicyAssignment.csv"
